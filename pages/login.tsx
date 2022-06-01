13cef432720c1e2d069c1eab61043163492e22c3
murat <m.ozturk@azerion.com>
2022-06-01 16:51:09 +0300

    Tailwind added

---
+++ pages/login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {NextPage} from "next";
+
+const Login: NextPage = () => {
+    return (
+        <div>
+            <h2>test</h2>
+        </div>
+    );
+};
+
+export default Login;
