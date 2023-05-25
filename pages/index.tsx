c9ad456a9c75f8ce92798a0ee7bfc0032f27c3c5
ozturk-murat <muratoztuurk@gmail.com>
2023-05-25 22:16:59 +0300

    api key

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,9 +19,9 @@ interface Props {
   documentaries: Movie[];
 }
 
-const Home = ({}: Props) => {
+const Home = ({netflixOriginals}: Props) => {
 
-    console.log("apiii", API_KEY);
+    console.log("apiii", netflixOriginals);
     
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
@@ -83,7 +83,7 @@ const Home = ({}: Props) => {
 
 export default Home;
 
-/* export const getServerSideProps = async () => {
+export const getServerSideProps = async () => {
   const [
     netflixOriginals,
     trendingNow,
@@ -108,4 +108,4 @@ export default Home;
         netflixOriginals: netflixOriginals.results,
     }
   }
-}; */
+};
